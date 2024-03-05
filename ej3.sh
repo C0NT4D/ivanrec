9ac3604b2dfe5f3182ee2c3301cf6633195e4db6
IvanContador <ivcoso@floridauniversitaria.es>
2024-03-05 10:09:22 +0100

    modificando 3

--- ej3.sh
+++ ej3.sh
@@ -11,14 +11,6 @@ read alumno
 echo "asignatura del alumno"
 read asignatura
 
-if [$nota -lt 5];then
-echo "Esta suspendido"
-elif [ $nota -gt 5 ];then
-echo "Aprobado"
-elif  [ $nota -eq 9] && [ $nota -eq 10 ] ; then
-echo "Ha sacado un sobresaliente"
-fi
-
 if [ "$alumno" -eq "$nombre" ] && [ "$asignatura" -eq "$modulo" ]; then 
 echo "Nota de $alumno en $asignatura es: $(TenerCalificacion $nota)"
 fi
