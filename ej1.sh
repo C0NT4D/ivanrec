4d4c8a708513ef61b42523f567653b19ade6cf11
IvanContador <ivcoso@floridauniversitaria.es>
2024-03-05 10:03:48 +0100

    cambiando ejercicio 1

--- ej1.sh
+++ ej1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 TenerCalificacion(){
-    echo "Introduce la nota del alumno"
+echo "Introduce la nota del alumno"
 read nota
-if [$nota < 5];then
+if [$nota -lt 5];then
 echo "Esta suspendido"
-elif [ $nota > 5 ];then
+elif [ $nota -gt 5 ];then
 echo "Aprobado"
-elif  [ $nota = 9 or  $nota = 10 ] ; then
+elif  [ $nota -eq 9] && [$nota -eq  10]; then
 echo "Ha sacado un sobresaliente"
 fi
 
